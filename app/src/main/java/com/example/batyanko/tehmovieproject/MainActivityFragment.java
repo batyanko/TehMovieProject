f257b45ae4cc7057d8661859e4f6204d1904ce14
Yanko <yanko1georgiev@gmail.com>
2016-02-21 22:35:30 +0200

    Picasso immage and internet access

--- app/src/main/java/com/example/batyanko/tehmovieproject/MainActivityFragment.java
+++ app/src/main/java/com/example/batyanko/tehmovieproject/MainActivityFragment.java
@@ -5,6 +5,9 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -19,8 +22,12 @@ public class MainActivityFragment extends Fragment {
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
-
-
+        Picasso.with(getActivity()).load("http://i.imgur.com/DvpvklR.png")
+                .into((ImageView) rootView.findViewById(R.id.teh_image));
+        
+        String s;
+        
         return rootView;
+        
     }
 }
